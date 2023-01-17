a65c843e4b6ae904d39ec75bf8d2a432dc171291
OlineRanum <oline.a.ranum@gmail.com>
2023-01-17 17:05:02 +0100

    Layout adjustment

--- plots.py
+++ plots.py
@@ -1,12 +1,15 @@
 import json
 import numpy as np
+import seaborn as sns 
 import matplotlib.pyplot as plt
 
 
 
 def figure_2():
+    custom_params = {"axes.spines.right": False, "axes.spines.top": False}
+    sns.set_theme(style="ticks", rc=custom_params)
 
-    fig, axs = plt.subplots(3, 6)
+    fig, axs = plt.subplots(3, 6, constrained_layout=True)
     metric_list = [['IIF', 'IID', 'IIR'], ['IGF','IGD','IGR'],  ['GIF','GID','GIR'],  ['GGF','GGD','GGR'],  ['AIF','AID','AIR'],  ['AGF','AGD','AGR']]
     x_values = ['8', '4', '2', '1', '1/2', '1/4', '1/8', 'ST']
 
@@ -21,10 +24,6 @@ def figure_2():
             axs[component, metric].bar(x_axis - 0.2, metrics)
             axs[component, metric].set_xticks(x_axis, x_values, rotation = 60)
             axs[component, metric].set_title(metric_list[metric][component])
-
-
-    fig.tight_layout()
     plt.show()
-
 
 figure_2()
