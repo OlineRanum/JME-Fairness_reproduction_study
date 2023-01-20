432c6ba615044be4078620d802f4d7be13d32d2f
OlineRanum <oline.a.ranum@gmail.com>
2023-01-20 10:52:14 +0100

    Auto stash before merge of "main" and "origin/main"

--- plots.py
+++ plots.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 
 custom_params = {"axes.spines.right": False, "axes.spines.top": False}
 sns.set_theme(style="ticks", rc=custom_params)
+sns.set_theme()
+sns.set(font_scale=1.5) 
 
 metric_list = [['IIF', 'IID', 'IIR'], ['IGF','IGD','IGR'],  ['GIF','GID','GIR'],  ['GGF','GGD','GGR'],  ['AIF','AID','AIR'],  ['AGF','AGD','AGR']]
 column_names = ["II", "IG", "GI", "GG", "AI", "AG"]
@@ -18,29 +20,41 @@ def min_max(arr):
     min_max = (arr-min_)/(max_-min_)
     return min_max
 
+def AUC(x, y):
+    area = 0
+    for i in range(len(x)-1):
+        area += (x[i+1]- x[i])*y[i+1] - 0.5*(x[i+1]- x[i])*(y[i+1]-y[i])
+    return area 
 
+def AUC_trap(x, y):
+    area = 0
+    for i in range(len(x)-1):
+        area += 0.5*(y[i+1]+ y[i])*(x[i+1]- x[i])
+    return area 
+
+
+def load_data(model, metric_list = metric_list):
 def load_data(model, metric_list = metric_list, fig4 = False):
     
     df = pd.DataFrame(columns = column_names)
     df["II"] = [[], [], []]
     for metric in range(len(metric_list)):
         for component in range(len(metric_list[metric])):
-            metrics = json.load(open('./save_exp/ml-1m/' + model + '/' + metric_list[metric][component]+ '_all_' + model + '_Y.json', 'r'))
-            static  = json.load(open('./save_exp/ml-1m/' + model + '/' + metric_list[metric][component] +'_all_' + model + '_static_Y.json', 'r'))
+            metrics = json.load(open('./save_exp/ml-1m/Experiment_1/' + model + '/' + metric_list[metric][component]+ '_all_' + model + '_Y.json', 'r'))
+            static  = json.load(open('./save_exp/ml-1m/Experiment_1/' + model + '/' + metric_list[metric][component] +'_all_' + model + '_static_Y.json', 'r'))
             if fig4: #static value not included
                 df[column_names[metric]][component] = metrics
             else:
                 metrics.extend(static)
                 metrics = min_max(np.array(metrics))
                 df[column_names[metric]][component] = metrics
-
     df.index = ['F', 'D', 'R']
     return df
 
 
 BPRMF = load_data('BPRMF')
-LDA = load_data('LDA')
-PureSVD = load_data('PureSVD')
+#LDA = load_data('LDA')
+#PureSVD = load_data('PureSVD')
 
 
 
@@ -58,14 +72,14 @@ def figure_2(model, column_names = column_names):
             axs[component, metric].set_xticks(x_axis, beta_values, rotation = 60)
             axs[component, metric].set_title(metric_list[metric][component])
 
-    plt.suptitle('Figure 2')
+    #plt.suptitle('Figure 2')
     #plt.savefig('Figure2.png', bbox_inches='tight')
     plt.show()
 
-#figure_2(BPRMF)
+figure_2(BPRMF)
 
 
-def figure_3(models, model_name = ['BPRMF', 'LDA', 'PureSVD'], column_names = column_names):
+def figure_3(models, model_name = ['BPRMF'], column_names = column_names):
     fig, axs = plt.subplots(1, 6, constrained_layout=True)
 
     x_axis = np.arange(len(x_values))
@@ -73,16 +87,17 @@ def figure_3(models, model_name = ['BPRMF', 'LDA', 'PureSVD'], column_names = co
     for i in range(len(models)):
         model = models[i]
         for metric in range(len(column_names)):
-            axs[metric].plot(model[column_names[metric]][1], model[column_names[metric]][2], label = model_name[i])
+            axs[metric].plot(model[column_names[metric]][1], model[column_names[metric]][2], '--*', label = model_name[i])
             axs[metric].set_xlabel(column_names[metric] + '-D')
             axs[metric].set_ylabel(column_names[metric] + '-R')
-
+            print('AUC ', column_names[metric],' = ', AUC(model[column_names[metric]][1], model[column_names[metric]][2]))
+            print('AUC trap', column_names[metric],' = ', AUC_trap(model[column_names[metric]][1], model[column_names[metric]][2]))
     # Put a legend below current axis
     plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),
           fancybox=True, shadow=True, ncol=5)
     plt.show()
 
-figure_3([BPRMF, LDA, PureSVD])
+#figure_3([BPRMF])
 
 BPRMF = load_data('BPRMF', fig4=True)
 LDA = load_data('LDA', fig4=True)
