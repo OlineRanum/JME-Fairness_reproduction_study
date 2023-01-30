35be4fb06baa50691cbb5b16629e4b1dd94223b4
OlineRanum <oline.a.ranum@gmail.com>
2023-01-30 20:31:32 +0100

    Added automatic download function for librarything

---
+++ src/datasets/download_librarything.py
@@ -0,0 +1,18 @@
+import gdown
+import tarfile 
+import os
+
+if os.path.exists('lt/reviews.txt'):
+    print('Dataset Librarything Already Downloaded ....')
+else:
+    url = 'https://drive.google.com/u/0/uc?id=1wgXv14TyrRD5DH6ZfJqApFymQv7_vuho&export=download'
+    output = 'lthing_data.tar.gz'
+    gdown.download(url, output, quiet = False)
+
+    mytar = tarfile.open(output)
+    mytar.extractall('./lt')
+    mytar.close()
+
+    os.rename('lt/lthing_data/reviews.txt', 'lt/reviews.txt')
+    os.remove('lt/lthing_data/edges.txt')
+    os.rmdir('lt/lthing_data')
