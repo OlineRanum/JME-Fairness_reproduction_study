2cee027c9793130af7b56019e1fcacde3c2b9d9d
OlineRanum <oline.a.ranum@gmail.com>
2023-02-03 20:40:13 +0100

    Edited setup text

--- env/setup.sh
+++ env/setup.sh
@@ -10,6 +10,7 @@ python3 src/datasets/download_librarything.py
 python << END
 print('------- JME-Fairness Reproducibility Study -------\n')
 print('Hello, welcome to the JME Reproducability Study code repository\n\n\
-To run all experiments:\n   \
-python3 src/run_experiments.sh \n')
+To plot all figures in our reproducibility study:\n   \
+conda activate jme\n
+bash experiments/run_files/plot_reproduction_results.sh \n')
 END
