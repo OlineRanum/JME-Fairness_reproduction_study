e26d89b86eb0fa340b2e75a56329d406bf017e5c
zhou-mian <mzhou050900@gmail.com>
2023-02-03 20:04:51 +0100

    Add Downloader

---
+++ src/BERT4Rec/download_pretrained.py
@@ -0,0 +1,22 @@
+import gdown
+import os
+from zipfile import ZipFile
+
+if os.path.exists('src/BERT4Rec/src/outputs/pths/ml-1m_10Epochs'):
+    print('Pretrained Model Already Downloaded ....')
+else:
+    print('Downloading Pretrained Models ....')
+    url = 'https://drive.google.com/uc?id=1gordPPgI7sxo8ZxnRa02PQeaG4r69dBn'
+    output = 'src/BERT4Rec/src/outputs/pths/'
+    gdown.download(url, output, quiet = False)
+
+    # loading the temp.zip and creating a zip object
+    with ZipFile(output+'JME_extension.zip', 'r') as zObject:
+        zObject.extractall(output)
+    zObject.close()
+
+    os.rename('src/BERT4Rec/src/outputs/pths/JME_extension/ml-1m_10Epochs', 'src/BERT4Rec/src/outputs/pths/ml-1m_10Epochs')
+    os.rename('src/BERT4Rec/src/outputs/pths/JME_extension/lt_10Epochs', 'src/BERT4Rec/src/outputs/pths/lt_10Epochs')
+    os.rmdir('src/BERT4Rec/src/outputs/pths/JME_extension')
+    os.remove('src/BERT4Rec/src/outputs/pths/JME_extension.zip')
+    print('Download of pretrained model completed')
